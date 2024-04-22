37185e2373f2238de3be93fb74aab61e1ee56be4
open dev <opendev.top@gmail.com>
2024-04-22 17:38:31 +0800

    Finished nextjs-ethers-metamask-connect

--- web3-metamask-connectors/nextjs-ethers-metamask-connect/my-app/pages/index.js
+++ web3-metamask-connectors/nextjs-ethers-metamask-connect/my-app/pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { ethers } from "ethers";
 import { useState } from "react";
 
 // connect to metamask
@@ -8,7 +9,7 @@ import { useState } from "react";
 
 export default function Home() {
   const [isConnected, setIsConnect] = useState(false);
-  const [provider, setProvider] = useState();
+  const [signer, setSigner] = useState();
 
   async function connect() {
     if (typeof window.ethereum !== "undefined") {
@@ -21,7 +22,7 @@ export default function Home() {
         let connectedProvider = new ethers.providers.Web3Provider(
           window.ethereum
         );
-        // setSigner(connectedProvider.getSigner());
+        setSigner(connectedProvider.getSigner());
       } catch (e) {
         console.log(e);
       }
@@ -30,13 +31,128 @@ export default function Home() {
     }
   }
 
+  async function execute() {
+    if (typeof window.ethereum !== "undefined") {
+      const contractAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+      const abi = [
+        {
+          inputs: [
+            {
+              internalType: "string",
+              name: "_name",
+              type: "string",
+            },
+            {
+              internalType: "uint256",
+              name: "_favoriteNumber",
+              type: "uint256",
+            },
+          ],
+          name: "addPerson",
+          outputs: [],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+        {
+          inputs: [
+            {
+              internalType: "string",
+              name: "",
+              type: "string",
+            },
+          ],
+          name: "nameToFavoriteNumber",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          name: "people",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "favoriteNumber",
+              type: "uint256",
+            },
+            {
+              internalType: "string",
+              name: "name",
+              type: "string",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [],
+          name: "retrieve",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [
+            {
+              internalType: "uint256",
+              name: "_favoriteNumber",
+              type: "uint256",
+            },
+          ],
+          name: "store",
+          outputs: [],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+      ];
+
+      // const provider = new ethers.providers.Web3Provider(window.ethereum);
+      // const signer = provider.getSigner();
+      const contract = new ethers.Contract(contractAddress, abi, signer);
+      try {
+        await contract.store(42);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      document.getElementById("executeButton").innerHTML =
+        "Please install MetaMask-plugin.";
+    }
+  }
+
   return (
     <div className={styles.container}>
       Hello Frogs!
       {isConnected ? (
-        "Connected!"
+        <>
+          {" "}
+          "Connected!"
+          <button id="executeButton" onClick={() => execute()}>
+            Execute
+          </button>
+        </>
       ) : (
-        <button onClick={() => connect()}>Connect</button>
+        <button id="connectButton" onClick={() => connect()}>
+          Connect
+        </button>
       )}
     </div>
   );
