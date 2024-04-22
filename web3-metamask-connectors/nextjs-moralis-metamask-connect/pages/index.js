caf32b5559920e4e526a3b077f4e53b6f39681de
open dev <opendev.top@gmail.com>
2024-04-22 23:21:16 +0800

    Finished nextjs-moralis-metamask-connect

--- web3-metamask-connectors/nextjs-moralis-metamask-connect/pages/index.js
+++ web3-metamask-connectors/nextjs-moralis-metamask-connect/pages/index.js
@@ -1,7 +1,31 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { useMoralis, useWeb3Contract } from "react-moralis";
+import { abi } from "../constants/abi";
 
 export default function Home() {
-  return <div className={styles.container}>Hiii!</div>;
+  const { enableWeb3, isWeb3Enabled } = useMoralis();
+
+  const { runContractFunction } = useWeb3Contract({
+    abi: abi,
+    contractAddress: "0x5fbdb2315678afecb367f032d93f642f64180aa3",
+    functionName: "store",
+    params: {
+      _favoriteNumber: 42,
+    },
+  });
+
+  return (
+    <div>
+      {isWeb3Enabled ? (
+        <>
+          "Connected! "
+          <button onClick={() => runContractFunction()}>Execute</button>
+        </>
+      ) : (
+        <button onClick={() => enableWeb3()}>Connect</button>
+      )}
+    </div>
+  );
 }
