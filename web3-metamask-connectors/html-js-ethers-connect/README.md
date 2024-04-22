950991a51deb699ee993fe8aa6ab6df2964fd25a
open dev <opendev.top@gmail.com>
2024-04-22 16:00:29 +0800

    Finished html follow

---
+++ web3-metamask-connectors/html-js-ethers-connect/README.md
@@ -0,0 +1,9 @@
+```shell
+yarn add ethers@5
+
+yarn add browserify
+
+yarn hardhat node
+yarn hardhat run ./scripts/deploy.js --network localhost
+yarn browserify index.js --standalone bundle -o ./dist/bundle.js
+```
---
+++ web3-metamask-connectors/html-js-ethers-connect/index.js
@@ -0,0 +1,117 @@
+const { ethers } = require("ethers");
+
+async function connect() {
+  if (typeof window.ethereum !== "undefined") {
+    console.log("We see metamask!");
+    await ethereum.request({
+      method: "eth_requestAccounts",
+    });
+  }
+}
+
+async function execute() {
+  // address √
+  // contrat ABI √
+  // function
+  // node connection √
+  const contractAddress = "0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0";
+  const abi = [
+    {
+      inputs: [
+        {
+          internalType: "string",
+          name: "_name",
+          type: "string",
+        },
+        {
+          internalType: "uint256",
+          name: "_favoriteNumber",
+          type: "uint256",
+        },
+      ],
+      name: "addPerson",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "string",
+          name: "",
+          type: "string",
+        },
+      ],
+      name: "nameToFavoriteNumber",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      name: "people",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "favoriteNumber",
+          type: "uint256",
+        },
+        {
+          internalType: "string",
+          name: "name",
+          type: "string",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "retrieve",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "_favoriteNumber",
+          type: "uint256",
+        },
+      ],
+      name: "store",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+  ];
+
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+  const contract = new ethers.Contract(contractAddress, abi, signer);
+  await contract.store(42);
+}
+
+module.exports = {
+  connect,
+  execute,
+};
