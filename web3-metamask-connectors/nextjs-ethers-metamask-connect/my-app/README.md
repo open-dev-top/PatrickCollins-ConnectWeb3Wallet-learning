e19ec0c6e9aee5fa17e300bbd3990695a9af4d5a
open dev <opendev.top@gmail.com>
2024-04-22 17:15:09 +0800

    Add connectButton

--- web3-metamask-connectors/nextjs-ethers-metamask-connect/my-app/README.md
+++ web3-metamask-connectors/nextjs-ethers-metamask-connect/my-app/README.md
@@ -1,5 +1,6 @@
 ```shell
 npx create-next-app@12
+yarn add ethers@5
 ```
 
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
--- web3-metamask-connectors/nextjs-ethers-metamask-connect/my-app/pages/index.js
+++ web3-metamask-connectors/nextjs-ethers-metamask-connect/my-app/pages/index.js
@@ -1,7 +1,43 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { useState } from "react";
+
+// connect to metamask
+// execute a funciton
 
 export default function Home() {
-  return <div className={styles.container}>Hello Frogs!</div>;
+  const [isConnected, setIsConnect] = useState(false);
+  const [provider, setProvider] = useState();
+
+  async function connect() {
+    if (typeof window.ethereum !== "undefined") {
+      console.log("We see metamask!");
+      try {
+        await ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setIsConnect(true);
+        let connectedProvider = new ethers.providers.Web3Provider(
+          window.ethereum
+        );
+        // setSigner(connectedProvider.getSigner());
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      setIsConnect(false);
+    }
+  }
+
+  return (
+    <div className={styles.container}>
+      Hello Frogs!
+      {isConnected ? (
+        "Connected!"
+      ) : (
+        <button onClick={() => connect()}>Connect</button>
+      )}
+    </div>
+  );
 }
